b9fdfa0f46d874f71be22a0ca1eb1fb703aa5c39
ILYA UTS <ilya.uts@jpmorgan.com>
2018-04-23 21:17:29 -0400

    Problem 15 solved

--- Problem15.js
+++ Problem15.js
@@ -13,32 +13,35 @@ let time = new Date().getTime(),
 /////////////// Code ///////////////
 let squarePathMap = {};
 
-for (let side = 1; side < 10; ++side) {
+const limit = 21;
+let nodes = Array.from(Array(limit), () => new Array(limit)),
+    allPaths = 0;
+
+// Create objects
+for (let h = 0; h < limit; ++h) {
+    for (let w = 0; w < limit; ++w) {
+        nodes[h][w] = {};
+    }
+}
+
+// Loop again to associate objects
+for (let h = limit - 1; h >= 0; --h) {
+    for (let w = limit - 1; w >= 0; --w) {
+        if (h > 0) {
+            nodes[h][w].down = nodes[h - 1][w];
+        }
+        if (w > 0) {
+            nodes[h][w].side = nodes[h][w - 1];
+        }
+        nodes[h][w].count = 0;
+    }
+}
+
+for (let side = 1; side < limit; ++side) {
     const sideLength = side;
 
-    let nodes = Array.from(Array(sideLength), () => new Array(sideLength)),
-        allPaths = 0;
 
-    // Create objects
-    for (let h = 0; h < sideLength; ++h) {
-        for (let w = 0; w < sideLength; ++w) {
-            nodes[h][w] = {};
-        }
-    }
-
-    // Loop again to associate objects
-    for (let h = sideLength - 1; h >= 0; --h) {
-        for (let w = sideLength - 1; w >= 0; --w) {
-            if (h > 0) {
-                nodes[h][w].down = nodes[h - 1][w];
-            }
-            if (w > 0) {
-                nodes[h][w].side = nodes[h][w - 1];
-            }
-        }
-    }
-
-    // Run the alogirthm
+    // Run the algorithm
     path({
         x: sideLength,
         y: sideLength
@@ -47,23 +50,26 @@ for (let side = 1; side < 10; ++side) {
 
     // Save path in square
     squarePathMap[sideLength] = allPaths;
-    console.log(squarePathMap);
+    // console.log(squarePathMap);
 
     function path(coord, currPath, isStart) {
         // If at the end, push to array
         if (coord.x == coord.y && coord.x == 0) {
             allPaths++;
-            return;
+
+            // Update all paths
+            for (let p of currPath) {
+                nodes[p.x][p.y].count++;
+            }
+            return 1;
         }
 
         // If you find yourself at a non initial square, just sum those paths from the previous iterations
-        if(coord.x == coord.y && !isStart) {
-            // console.log(coord.x, squarePathMap[coord.x]);
-            // console.log(coord, sideLength);
-            allPaths += squarePathMap[coord.x];
-            return;
+        if (nodes[coord.x][coord.y].count && !isStart) {
+            allPaths += nodes[coord.x][coord.y].count;
+            return nodes[coord.x][coord.y].count;
         }
-
+        // console.log(coord.x, coord.y);
         if (coord.x > 0) {
             let nextNode = {
                 x: coord.x - 1,
@@ -73,7 +79,8 @@ for (let side = 1; side < 10; ++side) {
             let newPath = currPath.slice();
             newPath.push(nextNode);
 
-            path(nextNode, newPath, false);
+            // If smaller path exists, no need to push it further
+            nodes[coord.x][coord.y].count += (nodes[nextNode.x][nextNode.y].count) ? nodes[nextNode.x][nextNode.y].count : path(nextNode, newPath, false);
         }
         if (coord.y > 0) {
             let nextNode = {
@@ -84,13 +91,15 @@ for (let side = 1; side < 10; ++side) {
             let newPath = currPath.slice();
             newPath.push(nextNode);
 
-            path(nextNode, newPath, false);
+            // If smaller path exists, no need to push it further
+            nodes[coord.x][coord.y].count += (nodes[nextNode.x][nextNode.y].count) ? nodes[nextNode.x][nextNode.y].count : path(nextNode, newPath, false);
         }
+
+        return nodes[coord.x][coord.y].count;
     }
 }
 
-// console.log(JSON.stringify(nodes));
-
+console.log('There are', nodes[nodes.length - 1][nodes.length - 1].count, 'paths in a', nodes.length - 1, 'x', nodes.length - 1, 'grid');
 ////////////////////////////////////
 
 let timeEnd = new Date().getTime();
