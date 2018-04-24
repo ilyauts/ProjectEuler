837e489c1d2b97e949f1b1511ed90eb08b7aee50
ILYA UTS <ilya.uts@jpmorgan.com>
2018-04-23 20:22:32 -0400

    Significantly increased speed but still way over a minute

--- Problem15.js
+++ Problem15.js
@@ -11,64 +11,81 @@ let time = new Date().getTime(),
     millisInSecond = 1000.;
 
 /////////////// Code ///////////////
-const sideLength = 14;
+let squarePathMap = {};
 
-let nodes = Array.from(Array(sideLength), () => new Array(sideLength)),
-    allPaths = 0;
+for (let side = 1; side < 10; ++side) {
+    const sideLength = side;
 
-// Create objects
-for (let h = 0; h < sideLength; ++h) {
-    for (let w = 0; w < sideLength; ++w) {
-        nodes[h][w] = {};
-    }
-}
+    let nodes = Array.from(Array(sideLength), () => new Array(sideLength)),
+        allPaths = 0;
 
-// Loop again to associate objects
-for (let h = sideLength-1 ; h >= 0; --h) {
-    for (let w = sideLength-1; w >= 0; --w) {
-        if(h > 0) {
-            nodes[h][w].down = nodes[h-1][w];
-        }
-        if(w > 0) {
-            nodes[h][w].side = nodes[h][w-1];
+    // Create objects
+    for (let h = 0; h < sideLength; ++h) {
+        for (let w = 0; w < sideLength; ++w) {
+            nodes[h][w] = {};
         }
     }
-}
 
-// Run the alogirthm
-path({
-    x: sideLength,
-    y: sideLength
-}, []);
-
-console.log(allPaths);
-
-function path(coord, currPath) {
-    // If at the end, push to array
-    if(coord.x == coord.y && coord.x == 0) {
-        allPaths++;
+    // Loop again to associate objects
+    for (let h = sideLength - 1; h >= 0; --h) {
+        for (let w = sideLength - 1; w >= 0; --w) {
+            if (h > 0) {
+                nodes[h][w].down = nodes[h - 1][w];
+            }
+            if (w > 0) {
+                nodes[h][w].side = nodes[h][w - 1];
+            }
+        }
     }
-    if(coord.x > 0) {
-        let nextNode = {
-            x: coord.x - 1,
-            y: coord.y
-        };
 
-        let newPath = currPath.slice();
-        newPath.push(nextNode);
+    // Run the alogirthm
+    path({
+        x: sideLength,
+        y: sideLength
+    }, [], true);
 
-        path(nextNode, newPath);
-    }
-    if(coord.y > 0) {
-        let nextNode = {
-            x: coord.x,
-            y: coord.y - 1
-        };
 
-        let newPath = currPath.slice();
-        newPath.push(nextNode);
+    // Save path in square
+    squarePathMap[sideLength] = allPaths;
+    console.log(squarePathMap);
 
-        path(nextNode, newPath);
+    function path(coord, currPath, isStart) {
+        // If at the end, push to array
+        if (coord.x == coord.y && coord.x == 0) {
+            allPaths++;
+            return;
+        }
+
+        // If you find yourself at a non initial square, just sum those paths from the previous iterations
+        if(coord.x == coord.y && !isStart) {
+            // console.log(coord.x, squarePathMap[coord.x]);
+            // console.log(coord, sideLength);
+            allPaths += squarePathMap[coord.x];
+            return;
+        }
+
+        if (coord.x > 0) {
+            let nextNode = {
+                x: coord.x - 1,
+                y: coord.y
+            };
+
+            let newPath = currPath.slice();
+            newPath.push(nextNode);
+
+            path(nextNode, newPath, false);
+        }
+        if (coord.y > 0) {
+            let nextNode = {
+                x: coord.x,
+                y: coord.y - 1
+            };
+
+            let newPath = currPath.slice();
+            newPath.push(nextNode);
+
+            path(nextNode, newPath, false);
+        }
     }
 }
 
